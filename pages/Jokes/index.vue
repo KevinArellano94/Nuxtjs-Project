bb10c481d48a14cd288ea32228b6697cea9cbc52
Kevin Arellano <Kevin.Arellano94@gmail.com>
2019-08-28 23:46:03 -0400

    .
    .

--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -62,9 +62,20 @@ export default {
 </script>
 
 <style scoped>
-.joke {
-  padding: 1rem;
-  border: 1px dotted #ccc;
-  margin: 1rem 0;
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+h1 {
+  font-weight: 400;
+}
+.name {
+  color: #00C48D;
+}
+p {
+  margin-top: 20px;
 }
 </style>
