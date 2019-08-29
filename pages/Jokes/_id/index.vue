8f23c82135fd6fc3a27a00cf2135afa483d6c327
Kevin Arellano <Kevin.Arellano94@gmail.com>
2019-08-28 23:41:53 -0400

    Jokes params added
    Parameters formula added.

---
+++ pages/Jokes/_id/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <NLink to="/jokes" class="button--grey">Back to Jokes</NLink>
+    <h2>{{ joke }}</h2>
+    <small>{{ $route.params.id }}</small>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      joke: {}
+    };
+  },
+  async created() {
+    const config = {
+      headers: {
+        Accept: "application/json"
+      }
+    };
+    try {
+      const res = await axios.get(
+        `https://icanhazdadjoke.com/j/${this.$route.params.id}`,
+        config
+      );
+
+      this.joke = res.data.joke;
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  head() {
+    return {
+      title: this.joke,
+      meta: [
+        {
+          hid: "Jokes",
+          name: "Jokes",
+          content: "Jokes"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+h1 {
+  font-weight: 400;
+}
+.name {
+  color: #00C48D;
+}
+p {
+  margin-top: 20px;
+}
+</style>
--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -62,20 +62,9 @@ export default {
 </script>
 
 <style scoped>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-h1 {
-  font-weight: 400;
-}
-.name {
-  color: #00C48D;
-}
-p {
-  margin-top: 20px;
+.joke {
+  padding: 1rem;
+  border: 1px dotted #ccc;
+  margin: 1rem 0;
 }
 </style>
